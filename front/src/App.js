d3159e20e3c198ec67c3d8d4d6bee2827c1bb584
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2022-10-16 12:28:31 -0500

    Inicializando del front ;)

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tienda More YL</h1>
+        <img src="./images/logo.png" alt="Logo de la tienda"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
