5ee26678c75223a76c89e3979aadb7bc3bcc20f0
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-02-20 08:59:37 -0500

    se implementaron validaciones en actualizar y recuperar contraseña, fix navbar

--- front/src/componentes/producto/ProductDetails.js
+++ front/src/componentes/producto/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import MetaData from "../layout/MetaData";
+import ReactHtmlParser from "react-html-parser";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
